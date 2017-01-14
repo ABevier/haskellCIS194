9f4865d6fcfdc78792db33925dd642e79aa1553f
Alan Bevier <alanbevier@Alans-MacBook-Pro.local>
2017-01-13 22:26:13 -0500

    Assignment 2

--- assignment2/LogAnalysis.hs
+++ assignment2/LogAnalysis.hs
@@ -39,13 +39,37 @@ insert :: LogMessage -> MessageTree -> MessageTree
 insert (Unknown _) n = n
 insert msg (Leaf) = Node Leaf msg Leaf
 insert msg (Node left value right) = case (compareLog msg value) of
-                                        True -> insert msg right
-                                        False -> insert msg left
+                                        True -> (Node left value (insert msg right))
+                                        False -> (Node (insert msg left) value right)
 
 
 compareLog :: LogMessage -> LogMessage -> Bool
 compareLog (LogMessage _ ts1 _) (LogMessage _ ts2 _) = ts1 > ts2
 compareLog _ _ = False
+
+
+inOrder :: MessageTree -> [LogMessage]
+inOrder Leaf = []
+inOrder (Node left value right) = (inOrder left) ++ value : (inOrder right)
+
+
+whatWentWrong :: [LogMessage] -> [String]
+whatWentWrong messages = doFilter $ inOrder(build messages)
+
+
+doFilter :: [LogMessage] -> [String]
+doFilter [] = []
+doFilter (x:xs) = case x of
+                       (LogMessage (Error n) _ message) | (n >= 50) -> message : (doFilter xs)
+                       _ -> doFilter xs
+
+
+
+
+testUtil :: IO ()
+testUtil = do
+          messages <- testParse parse 10 "error.log"
+          putStrLn $ show $ inOrder(build messages)
 
 
 main :: IO ()
