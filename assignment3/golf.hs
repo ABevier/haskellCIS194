413a7649c725ef3b29d4a155b6846136c37a9070
Alan Bevier <alanbevier@Alans-MacBook-Pro.local>
2017-01-15 22:51:26 -0500

    Assignment 3 - localMaxima

--- assignment3/golf.hs
+++ assignment3/golf.hs
@@ -17,4 +17,14 @@ doSkip xs n = map (\i -> snd i)
 --skipsN n iter (x:xs)
 --    | iter `mod` n == 0 = x : (skipsN n (iter + 1) xs)
 --    | otherwise = skipsN n (iter + 1) xs
---skipsN _ _ _ = []
+--skipsN _ _ _ = 
+
+
+localMaxima :: [Integer] -> [Integer]
+localMaxima x = map (\(x, y, z) -> y)
+             $ filter (\(x, y, z) -> y > x && y > z)
+             $ localMaximaXform x
+
+localMaximaXform :: [a] -> [(a, a, a)]
+localMaximaXform (x:y:z:xs) = (x,y,z) : (localMaximaXform $ y:z:xs)
+localMaximaXform _ = []
