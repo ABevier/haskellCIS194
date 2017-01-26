70815f14b9e84df75518384730e61bc172d8bec9
Alan Bevier <alanbevier@Alans-MacBook-Pro.local>
2017-01-26 01:24:23 -0500

    Permutations?

---
+++ playground/Permute.hs
@@ -0,0 +1,13 @@
+module Permute where
+
+--permutations :: String -> [String]
+--permutations xs = permutations' xs
+
+permute :: String -> [String]
+permute xs = [ n:ys | (n, ns) <- subStringTuple xs, ys <- permute ns]
+
+subStringTuple :: String -> [(Char, String)]
+subStringTuple [] = []
+subStringTuple (n:ns) = (n,ns) : [(x, n:xs) | (x, xs) <- subStringTuple ns]
+
+
